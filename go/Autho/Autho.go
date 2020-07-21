ddb070ece1d1d348d12474d9b972ebf3925831df
zach <zendrulat@gmail.com>
2020-07-21 07:53:03 -0500

    reformated

--- go/Autho/Autho.go
+++ go/Autho/Autho.go
@@ -7,22 +7,23 @@ import (
 	"github.com/gorilla/securecookie"
 	"github.com/gorilla/sessions"
 	. "github.com/logrusorgru/aurora"
+
+	//import
+	GetAllUsers "github.com/golangast/go_sapper/go/DB"
 )
 
+//initialize for autho
 func init() {
 	authKeyOne := securecookie.GenerateRandomKey(64)
 	encryptionKeyOne := securecookie.GenerateRandomKey(32)
-
 	Store = sessions.NewCookieStore(
 		authKeyOne,
 		encryptionKeyOne,
 	)
-
 	Store.Options = &sessions.Options{
 		MaxAge:   60 * 15,
 		HttpOnly: true,
 	}
-
 	gob.Register(User{})
 }
 
@@ -39,6 +40,7 @@ func CheckUser(s *sessions.Session) User {
 	val := s.Values["user"]
 	var user = User{}
 
+	//prints the cookie data
 	fmt.Println(BgRed("/ʕ◔ϖ◔ʔ/ Cookies````````````````````````````````````````````"))
 	fmt.Printf("Options:%v\n - Values:%v\n - Path:%s\n - ID:%v\n - Name:%d\n - IsNew:%v\n - Domain:%s\n - MaxAge:%d\n - User:%v/n",
 		Cyan(s.Options),
@@ -51,10 +53,47 @@ func CheckUser(s *sessions.Session) User {
 		BgBrown(s.Options.MaxAge),
 		BgMagenta(user))
 
+	//checking user autho
 	user, ok := val.(User)
 	if !ok {
 		fmt.Println("user not autho")
 		return User{Authenticated: false}
 	}
 	return user
+}
+
+//where the actual Autho begins
+func GetAuthoUser(Username string, pass string) *User {
+	fmt.Println("autho begin")
+	fmt.Println("return request")
+	//begininig authoa
+	login := GetAllUsers.GetAllUsers()
+	//begin scanning
+	for key, value := range login {
+		fmt.Println(key, " username is: ", value.Username, "password is: ", value.Password)
+		//comparing usernames
+		if value.Username != Username {
+			fmt.Println("user not found")
+		} else {
+			fmt.Println("user found! ", value.Username)
+			//check passwords
+			if pass != value.Password {
+				fmt.Println("password not found")
+			} else {
+				fmt.Println("password found! ", value.Password)
+				user := &User{
+					Username:      Username,
+					Authenticated: true,
+				}
+				fmt.Println("right before return ", user)
+				return user
+			}
+		}
+	}
+	user := &User{
+		Username:      Username,
+		Authenticated: false,
+	}
+	return user
+
 }
--- go/Handlers/Login/Logins.go
+++ go/Handlers/Login/Logins.go
@@ -6,85 +6,53 @@ import (
 	"net/http"
 	"text/template"
 
-	//3rd party
-
 	//imported files
 	Autho "github.com/golangast/go_sapper/go/Autho"
-	GetAllUsers "github.com/golangast/go_sapper/go/DB"
 )
-
+//template stuff
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.html"))
 }
-
-// tpl holds all parsed templates
 var tpl *template.Template
 
-// login authenticates the user
+// login handler authenticates the user
 func Logins(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("login started")
+
+	//checks is cookie is there
 	session, err := Autho.Store.Get(r, "cookie-name")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
+	//grab the form data
 	username := r.FormValue("username")
 	pass := r.FormValue("code")
-	user := getDBUser(username, pass)
-	fmt.Println(user, username, pass)
+	if pass == "" {
+		session.AddFlash("Must enter a code")
+	}
+	return
+}
+
+	//authorize
+	user := Autho.GetAuthoUser(username, pass)
+    //create session
 	session.Values["user"] = user
+	session.Values["authenticated"] = true
 	err = session.Save(r, w)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		fmt.Println("didnt make it to redirect")
 		return
 	}
+
+	//send them on their way
 	fmt.Println("you are authoed and now being redirected to secret", user)
-	session.Values["authenticated"] = true
-	session.Save(r, w)
 	fmt.Println("Your request method:", r.Method)
 	err = tpl.ExecuteTemplate(w, "secret.html", nil)
 	if err != nil {
 		log.Fatalln("template didn't execute: ", err)
 	}
 
-	if r.FormValue("code") == "" {
-		session.AddFlash("Must enter a code")
-	}
-	return
-}
 
-func getDBUser(Username string, pass string) *Autho.User {
-	fmt.Println("autho begin")
-	fmt.Println("return request")
-	//begininig authoa
-	login := GetAllUsers.GetAllUsers()
-	//begin scanning
-	for key, value := range login {
-		fmt.Println(key, " username is: ", value.Username, "password is: ", value.Password)
-		//comparing usernames
-		if value.Username != Username {
-			fmt.Println("user not found")
-		} else {
-			fmt.Println("user found! ", value.Username)
-			//check passwords
-			if pass != value.Password {
-				fmt.Println("password not found")
-			} else {
-				fmt.Println("password found! ", value.Password)
-				user := &Autho.User{
-					Username:      Username,
-					Authenticated: true,
-				}
-				fmt.Println("right before return ", user)
-				return user
-			}
-		}
-	}
-	user := &Autho.User{
-		Username:      Username,
-		Authenticated: false,
-	}
-	return user
-
-}
